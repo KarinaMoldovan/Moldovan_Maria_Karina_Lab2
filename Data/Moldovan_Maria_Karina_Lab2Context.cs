837a1fc46bcba11558411153a020640c54e05d6c
LocalAccount <karina.maria04@yahoo.com>
2024-10-16 20:09:36 +0300

    Add project files.

---
+++ Data/Moldovan_Maria_Karina_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moldovan_Maria_Karina_Lab2.Models;
+
+namespace Moldovan_Maria_Karina_Lab2.Data
+{
+    public class Moldovan_Maria_Karina_Lab2Context : DbContext
+    {
+        public Moldovan_Maria_Karina_Lab2Context (DbContextOptions<Moldovan_Maria_Karina_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Moldovan_Maria_Karina_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Moldovan_Maria_Karina_Lab2.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        [Display(Name = "Book Title")]
+        public String Title { get; set; }
+        public String Author {  get; set; }
+        [Column(TypeName ="decimal(6,2)")]
+        public decimal Price { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; } 
+
+    }
+}
