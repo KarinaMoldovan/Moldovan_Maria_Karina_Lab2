4e4941a93cc2ef1d6cdd37bf0401e1c448dc1f1d
LocalAccount <karina.maria04@yahoo.com>
2024-11-03 16:39:21 +0200

    Laborator4

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,8 @@
+﻿namespace Moldovan_Maria_Karina_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
